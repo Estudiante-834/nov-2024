df053746a89264841c32aa7ebc34f30f9d7837cc
Estudiante-834 <Estudiante_834@outlook.com>
2024-11-18 19:10:43 +0100

    ADD: Efecto sombra añadido a mensaje de consola

--- app/main.js
+++ app/main.js
@@ -1,3 +1,3 @@
-let style = "font-size: 100px; font-weight: bold; color: green;";
+let style = "font-size: 100px; font-weight: bold; color: green; text-shadow: 1px 1px 2px black;";
 
 console.log("%c Hello, WORLD!", style);
